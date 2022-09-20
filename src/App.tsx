f1c9205c26598c5af439ca56d3dda6753462c744
develop-gulin <gulin@rsinteractive.co.kr>
2022-09-20 18:16:07 +0900

    cleared

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 // import { MuiTextField } from "./componants/MuiTextField";
 // import { MuiSelect } from "./componants/MuiSelect";
 // import { MuiTable } from "./componants/MuiTable";
-// import { MuiPicker } from "./componants/MuiPicker";
+import { MuiPicker } from "./componants/MuiPicker";
 
 function App() {
   return (
@@ -16,7 +16,7 @@ function App() {
         {/* <MuiTextField /> */}
         {/* <MuiSelect /> */}
         {/* <MuiTable /> */}
-        {/* <MuiPicker /> */}
+        <MuiPicker />
       </div>
     </LocalizationProvider>
   );
